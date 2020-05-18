62ca8786e64baad3a387268b6c8be292a0c1f719
Bin Wang <binwang@cs.umass.edu>
2020-05-18 16:21:27 -0400

    Bump k3s and helm versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+K3S_VERSION="v1.17.5+k3s1"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} sh -s - --write-kubeconfig-mode 644
 
-HELM_VERSION="v2.14.1"
+HELM_VERSION="v3.2.1"
 OS_TYPE="linux"
 ARCH="amd64"
 HELM_PACKAGE="helm-${HELM_VERSION}-${OS_TYPE}-${ARCH}.tar.gz"
@@ -11,7 +12,6 @@ sudo mv helm/${OS_TYPE}-${ARCH}/helm /usr/local/bin
 
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.profile
 source ~/.profile
-helm init --wait
 
 OWCLI_VERSION="1.0.0"
 OWCLI_PACKAGE="OpenWhisk_CLI-${OWCLI_VERSION}-${OS_TYPE}-${ARCH}.tgz"
@@ -26,5 +26,4 @@ git clone https://github.com/wkk/openwhisk-deploy-kube.git
 sudo usermod -aG docker $USER
 newgrp docker
 
-kubectl create clusterrolebinding tiller-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 kubectl label nodes --all openwhisk-role=invoker
