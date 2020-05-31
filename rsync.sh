adcc531c117720fb26b1c1a7481f754ad84bb4bb
Bin Wang <jerry.binwang@gmail.com>
2020-05-30 23:29:51 -0400

    Update deployment script

--- rsync.sh
+++ rsync.sh
@@ -2,3 +2,6 @@
 rsync -avzhe "ssh -i edgewhisk.pem" \
 --exclude ".idea" --exclude ".gradle" \
 openwhisk/ ubuntu@(terraform output ip):openwhisk/
+
+rsync -avzhe "ssh -i edgewhisk.pem" \
+openwhisk-deploy-kube/ ubuntu@(terraform output ip):openwhisk-deploy-kube/
