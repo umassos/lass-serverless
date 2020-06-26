47389416442edaa0e4cdaeadb07b31da9269c003
Bin Wang <jerry.binwang@gmail.com>
2020-06-26 01:51:00 -0400

    Add mobilenet test action

---
+++ mobilenetv3-action/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build . -t binw/mnv3-action
+docker push binw/mnv3-action
--- rsync.sh
+++ rsync.sh
@@ -5,3 +5,8 @@ openwhisk/ ubuntu@(terraform output ip):openwhisk/
 
 rsync -avzhe "ssh -i edgewhisk.pem" \
 openwhisk-deploy-kube/ ubuntu@(terraform output ip):openwhisk-deploy-kube/
+
+rsync -avzhe "ssh -i edgewhisk.pem" \
+--exclude "v3-small_224_1.0_float.tgz" \
+mobilenetv3-action/ ubuntu@(terraform output ip):mobilenetv3-action
+
